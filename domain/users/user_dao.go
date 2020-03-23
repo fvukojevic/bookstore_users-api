3d58666b3a5ff9b41890fee9fdbb779660e16bc7
Ferdo Vukojević <f.vukojevic"globalsoft.ba>
2020-03-23 13:35:26 +0100

    save user in mysql db

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -11,10 +11,16 @@ import (
 	"github.com/fvukojevic/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/fvukojevic/bookstore_users-api/utils/date_utils"
 	"github.com/fvukojevic/bookstore_users-api/utils/errors"
+	"strings"
 )
 
 var (
 	usersDB = make(map[int64]*User)
+)
+
+const (
+	indexUnique     = "Duplicate entry"
+	queryInsertUser = "INSERT INTO users(first_name, last_name, email, date_created) VALUES(?, ?, ?, ?);"
 )
 
 func (user *User) Get() *errors.RestErr {
@@ -36,15 +42,26 @@ func (user *User) Get() *errors.RestErr {
 }
 
 func (user *User) Save() *errors.RestErr {
-	current := usersDB[user.Id]
-	if current != nil {
-		if current.Email == user.Email {
-			return errors.NewBadRequestError(fmt.Sprintf("email %s already registered", user.Email))
-		}
-		return errors.NewBadRequestError(fmt.Sprintf("user %d already exiists", user.Id))
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerError(err.Error())
 	}
+	defer stmt.Close()
 
 	user.DateCreated = date_utils.GetNowString()
-	usersDB[user.Id] = user
+	insertResult, err := stmt.Exec(user.FirstName, user.LastName, user.Email, user.DateCreated)
+	if err != nil {
+		if strings.Contains(err.Error(), indexUnique) {
+			return errors.NewBadRequestError(fmt.Sprintf("Email %s already exists", user.Email))
+		}
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when tryting to save the user: %s", err.Error()))
+	}
+	userId, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerError(
+			fmt.Sprintf("error when tryting to save the user: %s", err.Error()))
+	}
+	user.Id = userId
 	return nil
 }
